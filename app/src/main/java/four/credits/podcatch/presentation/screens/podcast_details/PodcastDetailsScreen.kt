2c87c843f3e28103a0f596d1db2454d273de0e84
Erik Grundy <erik.grundy@alstefgroup.com>
2024-05-14 17:45:55 +1200

    WIP enable download episodes from podcast details

--- app/src/main/java/four/credits/podcatch/presentation/screens/podcast_details/PodcastDetailsScreen.kt
+++ app/src/main/java/four/credits/podcatch/presentation/screens/podcast_details/PodcastDetailsScreen.kt
@@ -2,6 +2,7 @@ package four.credits.podcatch.presentation.screens.podcast_details
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.filled.Delete
@@ -12,7 +13,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -47,7 +50,8 @@ fun NavGraphBuilder.podcastDetailsScreen(
             viewModel.delete()
             onNavigateUp()
         },
-        onEpisodeClick = onEpisodeClick
+        onEpisodeClick = onEpisodeClick,
+        onDownload = { _ -> },
     )
 }
 
@@ -62,6 +66,7 @@ private fun PodcastDetailsScreen(
     podcast: Podcast,
     onDelete: () -> Unit,
     onEpisodeClick: (Long) -> Unit,
+    onDownload: (Episode) -> Unit,
 ) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(LocalSpacing.current.medium)
@@ -73,7 +78,11 @@ private fun PodcastDetailsScreen(
             Text(podcast.description)
         }
         items(items = podcast.episodes, key = { it.id }) {
-            EpisodeDisplay(episode = it, onClick = { onEpisodeClick(it.id) })
+            EpisodeDisplay(
+                episode = it,
+                onClick = { onEpisodeClick(it.id) },
+                onDownload = { onDownload(it) }
+            )
         }
         item {
             IconButton(onClick = onDelete) {
@@ -87,9 +96,32 @@ private fun PodcastDetailsScreen(
 }
 
 @Composable
-private fun EpisodeDisplay(episode: Episode, onClick: () -> Unit) {
+private fun EpisodeDisplay(
+    episode: Episode,
+    onClick: () -> Unit,
+    onDownload: () -> Unit,
+) {
     Card(modifier = Modifier.clickable { onClick() }) {
-        Text(episode.title, fontSize = 16.sp)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                episode.title,
+                fontSize = 16.sp,
+                modifier = Modifier.weight(1f),
+            )
+            if (episode.downloaded) {
+                Icon(
+                    painterResource(id = R.drawable.download_done),
+                    stringResource(id = R.string.download_completed)
+                )
+            } else {
+                IconButton(onClick = onDownload) {
+                    Icon(
+                        painterResource(id = R.drawable.download),
+                        stringResource(id = R.string.download_episode)
+                    )
+                }
+            }
+        }
         HorizontalDivider()
         Text(episode.description)
     }
@@ -116,11 +148,13 @@ private fun PodcastDetailsScreenPreview() {
                         description = "A cool description",
                         link = "",
                         id = 2,
+                        downloaded = true,
                     )
                 ),
             ),
             onDelete = {},
             onEpisodeClick = {},
+            onDownload = { _ -> },
         )
     }
 }
