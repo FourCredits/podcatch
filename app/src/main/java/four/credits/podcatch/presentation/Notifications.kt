f67219b67b5308a5512a6f57a75473ef9ec856cd
Erik Grundy <erik.grundy@alstefgroup.com>
2024-05-29 21:12:03 +1200

    add todo

--- app/src/main/java/four/credits/podcatch/presentation/Notifications.kt
+++ app/src/main/java/four/credits/podcatch/presentation/Notifications.kt
@@ -3,4 +3,5 @@ package four.credits.podcatch.presentation
 // player notification
 const val playerChannelId = "player"
 const val playerNotificationId = 1
+// TODO: move to string resources
 const val playerNotificationDescription = "Player Notifications"
